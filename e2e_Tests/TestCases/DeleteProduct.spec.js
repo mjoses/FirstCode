2eb07536df7e0432ed78a95eb9caa702cf2b56c2
mjoses <otmarac@gmail.com>
2023-11-13 19:02:01 +1300

    Commented and formatted test scripts

--- e2e_Tests/TestCases/DeleteProduct.spec.js
+++ e2e_Tests/TestCases/DeleteProduct.spec.js
@@ -7,9 +7,12 @@ import { NavBar } from '../../Pages/NavigationBar'
 import { BooksPage } from '../../Pages/BooksPage'
 import { CartPage } from '../../Pages/CartPage'
 
+
+//declare global variables
 var Link
 var page
 
+//Runs a scripts before the main test open and signs into the website
 test.beforeAll('Launch and Signin', async ({ browser }) => {
     page = await browser.newPage()
     Link = new NavLinks(page)
@@ -25,7 +28,7 @@ test.beforeAll('Launch and Signin', async ({ browser }) => {
     await newLogin.SignIn(username, password)
 })
 
-
+//Logs out and closes the wesbite
 test.afterAll('Sign out and Close', async ({ }) => {
 
     await Link.SignOut();
@@ -33,47 +36,44 @@ test.afterAll('Sign out and Close', async ({ }) => {
 
 })
 
-test('Add a Product', async ({ }) => {
 
+//This adds and deletes product from the cart
+test('Add a Product and delete', async ({ }) => {
+
+    //initiate and declare objects and variables
     const newNavBar = new NavBar(page)
     const newBook = new BooksPage(page)
     const newCart = new CartPage(page)
     let bookName
 
+    //iniate and declare assertion variables
+    const idLocator = await page.locator('td:nth-child(3)', { hasText: bookName });
+    const checkbox = await page.locator('table.cart tbody tr', { has: idLocator });
 
+    //Navigate to Books category
     await newNavBar.GoToACategory('Books')
 
+    //Adds the first book available in the cart from the category
     await newBook.AddFirstAvailableBook()
+
+    //Get the name of the added book
     bookName = await newBook.GetFirstAvailableBookName()
 
-    console.log('Product Name: ' + bookName)
+    //below is for debug purposes
+    //console.log('Product Name: ' + bookName)
+
+    //navigate to cart
     await Link.GotoCart()
 
-    const idLocator =  await page.locator('td:nth-child(3)', { hasText: bookName });
-    const checkbox =  await page.locator('table.cart tbody tr', { has: idLocator });
-
+    //assert if the added product is in the cart
     await expect(checkbox).toHaveCount(1)
     await expect(checkbox).toBeVisible()
-
-
-
-
-
-    //Assert if produc is added
 
     //delete product
     await newCart.deletedProduct(bookName)
 
     //Assert if product is deleted
-
     await expect(checkbox).toHaveCount(0)
-
-
-
-
-
-
-
 })
 
 
